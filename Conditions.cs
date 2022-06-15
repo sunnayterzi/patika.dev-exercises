3265187396dfd122b0b15f6c39648249adf6bece
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-16 02:02:36 +0300

    Add files via upload

---
+++ Conditions.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Notunuzu giriniz");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a<45) {
+    Console.WriteLine("Kaldiniz");
+}
+else if(45<=a && a<60) {
+    Console.WriteLine("Kosullu gectiniz");
+}
+
+else {
+    Console.WriteLine("Gectiniz");
+}
+
+string sonuc = a<45 ? "Kaldiniz" : a<60 && a>=45 ? "Kosullu gectiniz" : "Kaldiniz";
