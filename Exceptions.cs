3be4c4abfe0a6115559d71ed415c4b0c8acf3b85
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-16 01:35:24 +0300

    Add files via upload

---
+++ Exceptions.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+try
+{
+    Console.WriteLine("Please, enter a number");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("The entered number is " + a);
+}
+catch (System.Exception e)
+{
+    
+    Console.WriteLine(e);
+}
+
+finally {
+    Console.WriteLine("Operation was completed");
+}
