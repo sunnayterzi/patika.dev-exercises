b033006c066d93332045a4b2c5a5a2d463441063
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-19 00:17:04 +0300

    Add files via upload

---
+++ method_example.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+class method_example{
+    static void Main (string [] args){
+
+        topla(8,5);
+        cikar(8,5);
+        carp(8,5);
+        
+    }
+
+    static void topla(int a, int b){
+        Console.WriteLine("Toplam: " + (a+b));
+    }
+
+    static void cikar(int a, int b){
+        Console.WriteLine("Çıkarma: " + (a-b));
+    }
+
+    static void carp(int a, int b){
+        Console.WriteLine("Çarpım: " + (a*b));
+    }
+
+
+}
+
+
