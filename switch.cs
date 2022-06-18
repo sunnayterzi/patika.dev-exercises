9d982973dda10bc367b2db5a0b783ee2d07b8aa1
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-18 18:48:13 +0300

    Add files via upload

---
+++ switch.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Notunuz nedir? Birini giriniz (0-50-100)");
+int not = Convert.ToInt32(Console.ReadLine());
+
+switch(not) {
+    case 50 :
+        Console.WriteLine("Koşullu");
+        break;
+    case 0:
+        Console.WriteLine("Başarısız");
+        break;
+    case 100:
+        Console.WriteLine("Başarılı");
+        break;
+
+}
