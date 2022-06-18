33a87de19a02716c82284b18818ad2aa6bd8d12e
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-18 20:08:00 +0300

    Add files via upload

---
+++ Loops2.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 0;
+while (num<25) {
+    Console.WriteLine(num);
+    num++;
+}
+
+int [] numList = {1, 2, 3, 4};
+foreach(int i in numList){
+    Console.Write(i);
+}
