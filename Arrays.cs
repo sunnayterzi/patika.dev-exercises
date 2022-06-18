38e8b31139857eb6e60fe6f025a1bbc3d04f51b0
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-18 20:30:00 +0300

    Add files via upload

---
+++ Arrays.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please, enter size of the array");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] arr = new int[n];
+Random random = new Random();
+
+for(int i = 0; i<arr.Length; i++){
+    arr[i] = random.Next();
+}
+for(int i = 0; i<arr.Length; i++){
+    Console.Write(arr[i] + " ");
+}
+
+
+
+
