3937db656d888dacd5a0b4af98e2a0c7deb3647d
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-18 19:18:08 +0300

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir sayı giriniz");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i=2; i<=num; i++){
+       
+       if(num == 2) {
+         Console.WriteLine("Bu bir asal sayıdır");
+         break;
+       }
+
+    
+       if (num % i ==0) { 
+            Console.WriteLine("Bu asal sayı değil"); 
+            break;
+        }
+
+       else {
+        Console.WriteLine("Bu bir asal sayıdır");
+        break;
+       }
+    
+
+
+}
+
+
