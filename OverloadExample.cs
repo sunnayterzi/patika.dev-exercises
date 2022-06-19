96e68ec648c57e2708cfa5dab768f63eb19e9e83
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-19 13:59:04 +0300

    Add files via upload

---
+++ OverloadExample.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+class OverloadExample {
+    static void Main(string [] args){
+        ekranaYazdir(5);
+        ekranaYazdir("hi!");
+
+    }
+
+    static void ekranaYazdir(int value){
+        Console.WriteLine(value);
+    }
+
+    static void ekranaYazdir(string value){
+        Console.WriteLine(value);
+    }
+    
+}
