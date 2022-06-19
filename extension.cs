4fb505f62dcd305f1715b668bedd21614ea962bd
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-19 14:53:29 +0300

    Add files via upload

---
+++ extension.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+class extension{
+    static void Main(string [] args){
+
+        string sentence ="Hello everyone, I am Sunay Terzi";
+        Console.WriteLine(sentence.makeUpper());
+        Console.WriteLine(sentence.makeLower());
+         Console.WriteLine(sentence.removeSpace());
+
+    }
+}
+
+public static class Extension {
+    public static string makeUpper(this string param){
+        return param.ToUpper();
+    }
+    public static string removeSpace(this string param){
+        string [] arr = param.Split(" ");
+        return string.Join("",arr);
+    }
+
+    public static string makeLower(this string param){
+        return param.ToLower();
+    }
+
+}
