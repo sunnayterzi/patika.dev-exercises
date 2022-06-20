d953ac707b61a5b4fdab0990133e4677da9cb5a3
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-21 00:07:29 +0300

    Add files via upload

---
+++ mathMEthods.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Calculator {
+    public int power(int n, int p){
+        if(n<0 || p <0){
+            throw new Exception("n and p should be non-negative");
+        }
+        else
+        return Convert.ToInt32(Math.Pow(n, p));
+    } 
+}
+//Write your code here
+
+class Solution{
+    static void Main(String[] args){
+        Calculator myCalculator=new  Calculator();
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            string[] num = Console.ReadLine().Split();
+            int n = int.Parse(num[0]);
+            int p = int.Parse(num[1]); 
+            try{
+                int ans=myCalculator.power(n,p);
+                Console.WriteLine(ans);
+            }
+            catch(Exception e){
+               Console.WriteLine(e.Message);
+
+            }
+        }
+        
+        
+        
+    }
+}
