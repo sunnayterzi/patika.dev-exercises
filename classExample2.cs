1053087dc660879842837f39ea235c33c3f821a6
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-21 23:00:10 +0300

    Add files via upload

---
+++ classExample2.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class classExample2 {
+    static void Main(String [] args){
+
+        Worker worker1 = new Worker("Ayşe","Uyar", "İK");
+        worker1.displayInfo();
+
+    }
+}
+
+class Worker {
+    public string Name;
+    public string Surname;
+    public string Department;
+
+    public Worker(string name, string surname, string department){
+        this.Department=department;
+        this.Name = name;
+        this.Surname = surname;
+
+    }
+
+    public Worker(){
+
+    }
+
+    public void displayInfo(){
+        Console.WriteLine(Name + " " + Surname + " " + Department);
+    }
+}
+
+
+
+
+
+
+
