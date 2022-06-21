08490cce3d514ab95b623a7cb81dfee4768b03e1
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-22 00:49:06 +0300

    Add files via upload

---
+++ EnumExample.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static void Main(String [] args){
+        Console.WriteLine(Gunler.pazartesi);
+        Console.WriteLine((int)Gunler.pazar);
+
+    }
+}
+
+enum Gunler {
+    pazartesi=1,
+    salı,
+    çarşamba,
+    perşembe,
+    cuma,
+    cumartesi,
+    pazar
+}
