f5a4eadf6f8d3f72eda5de600d61d9947e0b4252
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-21 17:34:18 +0300

    Add files via upload

---
+++ ListExample.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class ListExample {
+
+    static void Main(String [] args) {
+
+        List<int> list1=new List<int>();
+        list1.Add(1);
+        list1.Add(25);
+        list1.Add(54);
+        list1.Add(74);
+        list1.Add(87);
+        list1.ForEach(num => Console.WriteLine(num));
+
+        list1.Remove(1);
+        list1.ForEach(num => Console.WriteLine(num));
+
+        list1.RemoveAt(3);
+        list1.ForEach(num => Console.WriteLine(num));
+
+        Console.WriteLine(list1.BinarySearch(87));
+
+        string [] animals = {"dog", "cat", "tiger", "bird"};
+        List <String> strList = new List<string>(animals);
+        strList.ForEach(str => Console.WriteLine(str));
+
+
+    }
+   
+
+}
+
+
+
