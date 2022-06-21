5a98ef10bb1c14e25c03128d270710796f0dfa79
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-22 00:14:59 +0300

    Add files via upload

---
+++ staticProperty.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static void Main(String [] args){
+
+        Console.WriteLine("Çalışan sayısı {0}", Calisan.CalisanSayisi);
+        Calisan calisan1 = new Calisan("Zeynep","Bastık","İK");
+        Console.WriteLine("Çalışan sayısı {0}", Calisan.CalisanSayisi);
+
+    }
+}
+
+class Calisan {
+    private static int calisanSayisi;
+    private string name;
+    private string surname;
+    private string departman;
+
+    public Calisan(string name, string surname, string departman)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.departman = departman;
+        calisanSayisi+=1;
+    }
+
+    static Calisan(){
+        calisanSayisi = 0;
+    }
+
+    public static int CalisanSayisi { get => calisanSayisi;  }
+}
