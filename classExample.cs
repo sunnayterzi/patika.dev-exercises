937644f88f9cca3f3401eb91b0c69e662ef6369a
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-21 22:26:31 +0300

    Add files via upload

---
+++ classExample.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+class Question1{
+
+    static void Main(String [] args) {
+        ArrayList list = new ArrayList();
+
+        Console.WriteLine("Bir cumle giriniz");
+        String str = Console.ReadLine();
+        
+            
+        for(int i = 0; i<str.Length;i++){
+            if(str[i] =='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U') 
+                list.Add(str[i]);
+        }
+
+        list.Sort();
+        foreach(var item in list){
+            Console.WriteLine(item);
+        }
+    }
+        
+
+}
+
+
+
+
+
+
