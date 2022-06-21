9fe19fd6bd62e2b48f2bc72244deea27780690b3
Sunay Terzi <46139477+sunnayterzi@users.noreply.github.com>
2022-06-21 23:53:38 +0300

    Add files via upload

---
+++ Encapsulation.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program {
+    static void Main(String [] args){
+
+        Ogrenci ogrenci1 = new Ogrenci("Elif", "Yalın", 29, 8);
+        ogrenci1.sinifAtlat();
+        ogrenci1.displayInfo();
+
+    }
+}
+
+class Ogrenci {
+    private string name;
+    private string surname;
+    private int no;
+    private int sinif;
+
+    public Ogrenci(String name, string surname, int no, int sinif){
+        this.name = name;
+        this.surname = surname;
+        this.no = no;
+        this.sinif = sinif;
+    }
+    
+
+    public string Name { get => name; set => name = value; }
+    public string Surname { get => surname; set => surname = value; }
+    public int No { get => no; set => no = value; }
+    public int Sinif { get => sinif; set => sinif = value; }
+
+    public void displayInfo(){
+        Console.WriteLine(name + " " + surname + " " + no + " " + " " + sinif);
+    }
+
+    public void sinifAtlat() {
+        this.Sinif+=1;
+    }
+}
